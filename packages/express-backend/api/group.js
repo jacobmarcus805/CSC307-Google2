a959c4f033a844e932d9f625e59451bc5cefc6ab
Travis B <travis.biehle@gmail.com>
2025-05-01 14:37:03 -0700

    created Group schema

---
+++ packages/express-backend/api/group.js
@@ -0,0 +1,32 @@
+import mongoose, { Schema } from "mongoose";
+
+const groupSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: false,
+    trim: false,
+  },
+  admins: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  ],
+  members: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  ],
+});
+
+const Group = mongoose.model("Group", groupSchema);
+
+export default Group;
