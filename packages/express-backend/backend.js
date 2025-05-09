e179f324418175529200d7c56836449861f1e2ae
Travis B <travis.biehle@gmail.com>
2025-05-09 12:01:06 -0700

    husky testing

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -140,7 +140,8 @@ app.post("/groups", (req, res) => {
     return;
   }
   console.log("Adding group:", groupToAdd);
-  groupServices.addGroup(groupToAdd)
+  groupServices
+    .addGroup(groupToAdd)
     .then(() => {
       res.status(201).send();
     })
@@ -153,3 +154,5 @@ app.post("/groups", (req, res) => {
 app.listen(port, (req, res) => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+// some changes
