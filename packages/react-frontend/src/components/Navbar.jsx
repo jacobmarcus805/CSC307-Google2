2b8b5cd017eacf657ef8161ed0d6900eef24a2c4
Nolan Knievel <nolan.knievel@icloud.com>
2025-05-12 11:22:19 -0700

    add basic navbar

---
+++ packages/react-frontend/src/components/Navbar.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { Box, Flex, Link, Button, Divider } from "@chakra-ui/react";
+import { Link as RouterLink } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <Box bg="green.700" px={4} py={2} color="white">
+      <Flex alignItems="center" justifyContent="center">
+        {/* App Name */}
+        <Link
+          as={RouterLink}
+          to="/login"
+          fontWeight="bold"
+          fontSize="lg"
+          color="white"
+          mr={8}
+        >
+          Poly Pups
+        </Link>
+
+        {/* Buttons grouped closer together */}
+        <Flex alignItems="center" gap={4}>
+          {/* Schedule Button */}
+          <Button
+            as={RouterLink}
+            to="/schedule/username"
+            colorScheme="green"
+            variant="ghost"
+            size="lg"
+            color="white"
+            _hover={{ bg: "green.600" }} // Change background color on hover
+          >
+            Schedule
+          </Button>
+          {/* Groups Button */}
+          <Button
+            as={RouterLink}
+            to="/groups/username"
+            colorScheme="green"
+            variant="ghost"
+            size="lg"
+            color="white"
+            _hover={{ bg: "green.600" }} // Change background color on hover
+          >
+            Groups
+          </Button>
+          {/* Login Button */}
+          <Button
+            as={RouterLink}
+            to="/login"
+            colorScheme="green"
+            variant="ghost"
+            size="lg"
+            color="white"
+            _hover={{ bg: "green.600" }} // Change background color on hover
+          >
+            Login
+          </Button>
+        </Flex>
+      </Flex>
+    </Box>
+  );
+}
+
+export default Navbar;
