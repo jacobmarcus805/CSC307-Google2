5e97cb41c34cc1e56001c809c84ad257a73d0644
Nolan Knievel <nolan.knievel@icloud.com>
2025-05-13 14:08:10 -0700

    add signup page to router

--- packages/react-frontend/src/routes/AppRoutes.jsx
+++ packages/react-frontend/src/routes/AppRoutes.jsx
@@ -3,11 +3,13 @@ import { Routes, Route } from "react-router-dom";
 import Login from "../pages/login";
 import Schedule from "../pages/schedule";
 import Groups from "../pages/groups";
+import Signup from "../pages/signup";
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
       <Route path="/schedule/:username" element={<Schedule />} />
       <Route path="/groups/:username" element={<Groups />} />
     </Routes>
