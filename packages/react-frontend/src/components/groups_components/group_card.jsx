6e224ca09704ac20a0d40482b73ed410ae8051b3
Travis B <travis.biehle@gmail.com>
2025-05-29 15:24:37 -0700

    group page with bogus data

---
+++ packages/react-frontend/src/components/groups_components/group_card.jsx
@@ -0,0 +1,70 @@
+import {
+  Icon,
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionPanel,
+  AccordionItem,
+  Card,
+  CardHeader,
+  CardBody,
+  Heading,
+  Text,
+  Button,
+  Flex,
+} from "@chakra-ui/react";
+import { FaUserCircle } from "react-icons/fa";
+import { FaUserGroup } from "react-icons/fa6";
+
+function GroupCard({ group }) {
+  const listMembers = (members) => {
+    return members.map((member, idx) => <p key={idx}>{member}</p>);
+  };
+
+  return (
+    <Card w="md" variant={"outline"} bg={"green.100"}>
+      <CardBody>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            gap: "0.5em",
+            paddingBottom: "0.5em",
+          }}
+        >
+          <Icon as={FaUserGroup} boxSize={"1.5em"} alignContent={"center"} />
+          <Heading size="md" mb={1}>
+            {group.name}
+          </Heading>
+        </div>
+        <Text>{group.description}</Text>
+        <Accordion
+          allowToggle
+          variant={"subtle"}
+          m={"0.5em"}
+          bg="green.200"
+          borderRadius={"md"}
+          border={"transparent"}
+        >
+          <AccordionItem>
+            <AccordionButton gap={2} alignItems={"center"}>
+              <Icon as={FaUserCircle} boxSize={5} />
+              <Text flex={1} textAlign={"left"}>
+                Members
+              </Text>
+              <AccordionIcon />
+            </AccordionButton>
+            <AccordionPanel>{listMembers(group.members)}</AccordionPanel>
+          </AccordionItem>
+        </Accordion>
+        <Flex placeContent={"flex-end"}>
+          <Button color={"red"} size={"sm"} variant={"ghost"}>
+            Leave
+          </Button>
+        </Flex>
+      </CardBody>
+    </Card>
+  );
+}
+
+export default GroupCard;
--- packages/react-frontend/src/pages/groups.jsx
+++ packages/react-frontend/src/pages/groups.jsx
@@ -1,12 +1,58 @@
 import React from "react";
+import GroupCard from "../components/groups_components/group_card";
+import { Heading, SimpleGrid } from "@chakra-ui/react";
 
-function Groups() {
+const groups_data = [
+  {
+    name: "Cal Poly Guide Dogs",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed volutpat ante ac sem consectetur convallis nec eget enim. Nullam volutpat tortor a eleifend scelerisque. Curabitur urna est, rutrum vel velit ut, gravida mattis mi. Fusce sagittis velit id odio mattis dignissim. Donec at varius elit.",
+    admins: ["id1", "id2"],
+    members: ["id3", "id4"],
+  },
+  {
+    name: "SLO Guide Dogs",
+    description:
+      "Nunc nec velit ut libero vestibulum sodales at at tortor. In eleifend turpis ut volutpat egestas. Ut quis nisi est. Nulla nisl turpis, euismod ac lacus in, luctus aliquet sem.",
+    admins: ["id5", "id6"],
+    members: ["id7", "id8"],
+  },
+  {
+    name: "California Guide Dogs",
+    description:
+      "Quisque vel consequat ipsum, eget interdum dui. Suspendisse tristique, nisl vitae aliquet consectetur, enim metus eleifend metus, eu consectetur lacus felis at justo.",
+    admins: ["id9"],
+    members: ["id10"],
+  },
+];
+
+const ListGroups = ({ groups }) => {
+  return (
+    <SimpleGrid
+      columns={1} // Responsive columns: 2 on small screens, 3 on larger screens
+      spacing={"2em"}
+      spacingX={"2em"}
+      justifyItems={"center"}
+      alignItems={"center"}
+      justifyContent={"center"}
+      paddingBottom={"2em"}
+    >
+      {groups.map((group) => (
+        <GroupCard key={group.name} group={group} />
+      ))}
+    </SimpleGrid>
+  );
+};
+
+const Groups = () => {
   return (
     <div>
-      <h1>Groups Page</h1>
-      <p>This is a placeholder for the Groups page.</p>
+      <Heading textAlign="center" padding={"1em"}>
+        Your Groups
+      </Heading>
+      <ListGroups groups={groups_data} />
     </div>
   );
-}
+};
 
 export default Groups;
