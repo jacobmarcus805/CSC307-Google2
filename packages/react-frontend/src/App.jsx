e1e8cda5330de42ebfacc047f335c06736a5a2e3
Jfrends <jfrends10@gmail.com>
2025-05-30 13:20:38 -0700

    added auth for group admin page, table to show members and remove them from your group, and fixed linter issues

--- packages/react-frontend/src/App.jsx
+++ packages/react-frontend/src/App.jsx
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 import Navbar from "./components/Navbar";
 
--- packages/react-frontend/src/pages/manageGroup.jsx
+++ packages/react-frontend/src/pages/manageGroup.jsx
@@ -1,13 +1,14 @@
-import { useState, useEffect } from "react";
-
+import React, { useState, useEffect } from "react";
+import MembersTable from "../components/membersTable";
 import { useParams } from "react-router-dom";
 
 function ManageGroup() {
-  const { groupId } = useParams();
+  const { userId, groupId } = useParams();
   const [group, setGroup] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  //const [users, setUsers] = useState(null);
+  const [members, setMembers] = useState(null);
+  const token = localStorage.getItem("token");
   useEffect(() => {
     async function fetchGroup() {
       try {
@@ -18,18 +19,69 @@ function ManageGroup() {
           throw new Error(`Failed to fetch group: ${response.status}`);
         const data = await response.json();
         setGroup(data);
-        //setUsers(data["members"]);
+        const memberFetches = data.members.map((id) =>
+          fetch(`${import.meta.env.VITE_API_BASE_URL}/users/${id}`, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }).then((res) => res.json()),
+        );
+        const fullMembers = await Promise.all(memberFetches);
+        setMembers(fullMembers);
       } catch (err) {
         setError(err.message);
         console.log(error);
       } finally {
-        console.log(loading);
         setLoading(false);
       }
     }
     fetchGroup();
   }, [groupId]);
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  if (!group.admins.some((admin) => admin.toString() === userId)) {
+    return (
+      <p>
+        You do not have permission to view this page because you are not an
+        admin.
+      </p>
+    );
+  }
   console.log(group);
+  console.log(members);
+
+  function removeOneMember(index) {
+    const updatedMembers = members.filter((_, i) => i !== index);
+    const url = `${import.meta.env.VITE_API_BASE_URL}/groups/` + groupId;
+    const updatedGroup = { ...group, members: updatedMembers };
+    fetch(url, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedGroup),
+    })
+      .then((res) => {
+        if (res.status === 200) {
+          setMembers([...updatedMembers]);
+        } else {
+          console.error(
+            "Request status not OK, instead server gave ",
+            res.status,
+          );
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  return (
+    <div className="container">
+      <MembersTable memberData={members} removeMember={removeOneMember} />
+    </div>
+  );
 }
 
 export default ManageGroup;
