fb87b48512647f9f3f3a73309eebf4b27a7f2f1f
Jacob Marcus <jacobmarcus@Jacobs-MacBook-Air-776.local>
2025-06-03 13:12:27 -0700

    Fixed signup with isAdmin issue on mongo

--- packages/express-backend/schemas/user.js
+++ packages/express-backend/schemas/user.js
@@ -50,10 +50,6 @@ const UserSchema = new mongoose.Schema({
       required: true,
     },
   ],
-  is_admin: {
-    type: Boolean,
-    required: true,
-  },
 });
 
 const User = mongoose.model("User", UserSchema);
